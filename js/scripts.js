800f678f96891c82549fd5bf221b3512483d623c
prospersimi <logicprime001@gmail.com>
2021-08-20 09:44:11 +0100

    add constructive function for addressbook which mocks a database for storing contact objects.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-/*
+
 // Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = {};
   this.currentId = 0;
 }
-
+/*
 AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
