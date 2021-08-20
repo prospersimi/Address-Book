19b7e4f2acd9d81b6e62ede3680156f4ee41406b
prospersimi <logicprime001@gmail.com>
2021-08-20 09:47:42 +0100

    added a prototypal method assignId which works with the addContact method to the constructor AddressBook.

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 };
-/*
+
 AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
-
+/*
 AddressBook.prototype.findContact = function (id) {
   if (this.contacts[id] != undefined) {
     return this.contacts[id];
